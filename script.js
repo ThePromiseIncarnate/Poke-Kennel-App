e035632287b5bf94fa879033368707006e6445af
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-09-03 20:17:54 -0500

    Fetch Data from the PokeAPI

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function submitPokedex() {
+async function submitPokedex() {
     console.log("Proof of life");
+    let pokedexValue = pokeID.value;
+    let pokedexFetch = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokedexValue}`);
+    console.log(pokedexFetch);
+    let pokedexData = await pokedexFetch.json();
+    console.log(pokedexData);
 }
 
 pokemonButton.addEventListener("click", submitPokedex);
