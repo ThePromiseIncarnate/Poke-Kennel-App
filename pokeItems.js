3048a8ca50b92a0e7d120377a0b032e42b61589f
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-09-05 21:05:52 -0500

    Process and Add Translations

--- pokeItems.js
+++ pokeItems.js
@@ -1,8 +1,23 @@
 console.log("Hello from poke-items.js!")
+let itemText = ``;
 
-function submitItem() {
+async function submitItem() {
     let itemValue = itemID.value;
     console.log(itemValue);
+    let itemFetch = await fetch(`https://pokeapi.co/api/v2/item/${itemValue}/`);
+    console.log(itemFetch);
+    let itemData = await itemFetch.json();
+    let itemNames = itemData.names;
+
+    let itemArray = itemData.name;
+    itemEntry.textContent = itemArray;
+
+    for (let i = 0; i < itemNames.length; i++) {
+        let itemList = itemNames[i];
+        itemText += `<li>${itemList.name}</li> \n`;
+    }
+    console.log(itemText);
+    itemEntry.innerHTML = itemText;
 }
 
 itemButton.addEventListener("click", submitItem);
