829cce8b4f1ad98ce8b21b2732c922e53c0843d8
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-09-05 19:46:29 -0500

    Create the HTML Form

--- pokeItems.js
+++ pokeItems.js
@@ -1,1 +1,8 @@
 console.log("Hello from poke-items.js!")
+
+function submitItem() {
+    let itemValue = itemID.value;
+    console.log(itemValue);
+}
+
+itemButton.addEventListener("click", submitItem);
